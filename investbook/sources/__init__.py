6b80376a7712dca88d5b15919e59198b398694e5
MateoSaezMata <msaez@triplealpha.in>
2024-11-21 17:25:22 +0100

    Source: fmp. Selecting endpoints

--- investbook/sources/__init__.py
+++ investbook/sources/__init__.py
@@ -1,14 +1,14 @@
 from investbook.sources.polygon import PolygonAPI
 
-class DataSources:
+class AssetsAPI:
 
     def __init__(self, polygon_api_key: str, avantage_api_key:str) -> None:
-        self.polygon_api_key = polygon_api_key
-        self.avantage_api_key = avantage_api_key
+        self._polygon_api_key = polygon_api_key
+        self._avantage_api_key = avantage_api_key
 
     @property
     def polygon(self):
-        return PolygonAPI(self.polygon_api_key)
+        return PolygonAPI(self._polygon_api_key)
     
     @property
     def alphavantage(self):
---
+++ investbook/sources/fmp/__init__.py
@@ -0,0 +1,17 @@
+'''
+https://site.financialmodelingprep.com/developer/docs#symbol-list-stock-list
+
+https://site.financialmodelingprep.com/developer/docs#company-profile-company-information
+
+https://site.financialmodelingprep.com/developer/docs#company-logo
+
+https://site.financialmodelingprep.com/developer/docs#stock-peers-company-information
+
+https://site.financialmodelingprep.com/developer/docs#full-quote-quote
+
+https://site.financialmodelingprep.com/developer/docs#stock-price-change-quote
+
+https://site.financialmodelingprep.com/developer/docs#income-statements-financial-statements
+
+https://site.financialmodelingprep.com/developer/docs#dividends-historical-dividends
+'''
