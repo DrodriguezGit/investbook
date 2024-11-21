b986e6e68ecb116266309bbb0202511824866821
David Rodriguez <drodriguez@triplealpha.in>
2024-11-21 15:57:50 +0100

    Source: Polygon. Add new endpoints

--- investbook/sources/polygon/__init__.py
+++ investbook/sources/polygon/__init__.py
@@ -50,15 +50,36 @@ class PolygonQueryManager:
 
 class PolygonAPI(PolygonQueryManager):
 
-    def get_tickers(self, type: str='ETF') -> dict:
+    def get_tickers(self,market: str, type: str, search: str) -> dict: #Pilla los tickets de todos los tipos de activos
         '''
         https://polygon.io/docs/stocks/get_v3_reference_tickers
         '''
-        return self.get('/v3/reference/tickers', type=type)
+        return self.get('/v3/reference/tickers/', market = market, type=type, search=search)
     
-    def get_price(self, ticker: str, date: str) -> dict:
+    def get_price_stock(self, ticker: str, date: str) -> dict:
         '''
         https://polygon.io/docs/stocks/get_v1_open-close__stocksticker___date
         '''
         return self.get(f'/v1/open-close/{ticker}/{date}')
     
+    def get_option_contract(self, underlying_ticker: str) -> dict:
+        
+        return self.get(f'v3/reference/options/contracts/{underlying_ticker}')
+    
+    def get_price_option(self, optionsTicker: str, date: str) -> dict:
+        '''
+        https://polygon.io/docs/options/get_v1_open-close__optionsticker___date
+        '''
+        return self.get(f'/v1/open-close/{optionsTicker}/{date}')
+    
+    def get_price_indice(self, indicesTicker: str, date: str) -> dict:
+        '''
+        https://polygon.io/docs/indices/get_v1_open-close__indicesticker___date
+        '''
+        return self.get(f'/v1/open-close/{indicesTicker}/{date}')
+    
+    def get_price_crypto(self, crypto: str, currency: str, date: str) -> dict:
+        '''
+        https://polygon.io/docs/crypto/get_v1_open-close_crypto__from___to___date
+        '''
+        return self.get(f'/v1/open-close/crypto/{crypto}/{currency}/{date}')
--- tests/polygon.py
+++ tests/polygon.py
@@ -1,23 +1,34 @@
-API_KEY='1DWuCVNmdWPBcfL8R9pffQ9UacVGA9lK'
-
 from investbook.sources.polygon import PolygonAPI
+from pprint import pprint
 
+API_KEY='1DWuCVNmdWPBcfL8R9pffQ9UacVGA9lK'
 api = PolygonAPI(api_key=API_KEY)
 
-r = api.get_tickers()
-
-from pprint import pprint
-pprint(r)
-
-
-#####
-
-from investbook.sources import DataSources
+t = api.get_tickers(market='', type='', search='tesla') 
+# Markets = stocks, crypto, fx, otc, indices   / search: Si no sabes el nombre de la empresa, busca coincidencias
+# pprint(t)
+print(f"Count: {t['count']}")
+for r in t['results']:
+    print(f"Name: {r['name']}, Ticker: {r['ticker']}, Market: {r['market']}")
 
 
-sources = DataSources()
+s = api.get_price_stock('TSLA', '2024-11-18')
+# Stocks prices (ticker, date)
+pprint(f"Close: {s['close']}, High: {s['high']}")
 
 
-sources.polygon.get_tickers()
+oc = api.get_option_contract('TSLZ')
+pprint(oc)
+o = api.get_price_option('TSLZ', '2024-11-12')
+# Option prices (ticketr, date)#################### Ahondar en los contratos de opciones
+pprint(o)
 
-sources.alphavantage
+
+i = api.get_price_indice('I:AGQIV', '2023-03-10')
+# Indices prices (I:ticker, date)
+pprint(i)
+
+
+c = api.get_price_crypto('BTC', 'EUR', '2024-11-04')
+# Crypto prices (crypto, currency, date)
+pprint(f"Precio de apertura: {c['open']},Precio de cierre: {c['close']}")
